da4da0f575e085c768dbea95a1a31f20136a0f95
ibrogg <ouagague.ibrahim@gmail.com>
2024-08-13 14:44:25 +0100

    Fix basename

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tmovies">
       <ApiProvider api={tmdbApi}>
         <ThemeProvider>
           <GlobalContextProvider>
