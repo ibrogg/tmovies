1bf7b5b4b0078b8b1ab85ea0639f8e79c7dd93fc
Ibrahim Ouagague <ouagague.ibrahim@gmail.com>
2024-10-30 09:08:04 +0000

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
-export const TMDB_API_BASE_URL: string | undefined = import.meta.env
+export const TMDB_API_BASE_URL: string = import.meta.env
   .VITE_TMDB_API_BASE_URL;
-export const API_KEY: string | undefined = import.meta.env.VITE_API_KEY;
+export const API_KEY: string = import.meta.env.VITE_API_KEY;
 
 export const THROTTLE_DELAY = 150;
