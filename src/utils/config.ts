1911484fccded08c5ac01885a2e7780eef3d9ee6
ibrogg <ouagague.ibrahim@gmail.com>
2024-10-30 10:01:42 +0000

    vf

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,6 @@
-export const TMDB_API_BASE_URL: string = import.meta.env
-  .VITE_TMDB_API_BASE_URL;
-export const API_KEY: string = import.meta.env.VITE_API_KEY;
+export const TMDB_API_BASE_URL: string =
+  import.meta.env.VITE_TMDB_API_BASE_URL || " https://api.themoviedb.org/3";
+export const API_KEY: string =
+  import.meta.env.VITE_API_KEY || "53fa4567228eb6a97287c478fa7ba2d7";
 
 export const THROTTLE_DELAY = 150;
